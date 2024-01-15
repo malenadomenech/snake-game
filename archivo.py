f1f65ec4fe9e66bde9939016920cb505a91017ac
malenadomenech <domenechmalena0@gmail.com>
2024-01-15 12:24:36 -0300

    creación ventana gráfica y cabeza de serpiente

--- archivo.py
+++ archivo.py
@@ -1,10 +1,28 @@
 import turtle
 from turtle import *
 
+#Configuración de la ventana
+
 wn = turtle.Screen()
 wn.title("Snake Game")
 wn.bgcolor("orange")
 wn.setup(width= 600, height= 600)
 wn.tracer(0)
+
+
+#Cabeza de la serpiente
+cabeza = turtle.Turtle()
+cabeza.speed(0)
+cabeza.shape("square")
+cabeza.color("black")
+cabeza.penup()
+cabeza.goto(0,0)
+cabeza.direction = "stop"
+
+
+
+while True:
+    wn.update()
+
  
-turtle.mainloop()
+
