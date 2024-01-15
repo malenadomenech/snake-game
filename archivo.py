edf964f885e7bca76e21e2127def2d049aad7696
malenadomenech <domenechmalena0@gmail.com>
2024-01-15 12:55:06 -0300

    creación de funciones para movimiento

--- archivo.py
+++ archivo.py
@@ -1,5 +1,8 @@
 import turtle
 from turtle import *
+import time
+ 
+posponer = 0.1
 
 #Configuración de la ventana
 
@@ -17,12 +20,37 @@ cabeza.shape("square")
 cabeza.color("black")
 cabeza.penup()
 cabeza.goto(0,0)
-cabeza.direction = "stop"
+cabeza.direction = "left"
+
+#Funciones
+def mov():
+    if cabeza.direction == "up":
+        y = cabeza.ycor()
+        cabeza.sety(y + 20)
+    
+    if cabeza.direction == "down":
+        y = cabeza.ycor()
+        cabeza.sety(y - 20)
+    
+    if cabeza.direction == "left":
+        x = cabeza.xcor()
+        cabeza.setx(x - 20)
+
+    if cabeza.direction == "right":
+        x = cabeza.xcor()
+        cabeza.setx(x + 20)
+
+    
 
 
 
 while True:
+    mov()
     wn.update()
+
+time.sleep(posponer)
+    
+
 
  
 
